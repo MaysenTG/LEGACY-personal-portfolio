bf07f13a7553ccddd648ee3aee43447515e3ef94
MaysenTG <greenwood.maysen@gmail.com>
2021-08-09 00:06:46 +1200

    change styling of list in projects to be more scalable. add new project

--- src/PageComponents/my-projects.js
+++ src/PageComponents/my-projects.js
@@ -1,6 +1,7 @@
 import TheTeaCozy from '../media/tea-cozy-website.jpg';
-import TheVCExp from '../media/VC-experience-website.jpg'
-import SprMktPrice from '../media/coding-photo-price-finder.webp'
+import TheVCExp from '../media/VC-experience-website.jpg';
+import SprMktPrice from '../media/coding-photo-price-finder.webp';
+import CmpdIntrst from '../media/compound interest app.png';
 
 function MyProjects() {
     return (
@@ -33,6 +34,17 @@ function MyProjects() {
             <ul>
               <li>Python</li>
               <li>Selenium</li>
+            </ul>
+          </div>
+          
+          <h2 className="project-heading apps">Apps</h2>
+          <div id="project-4" className="project-container">
+            <h3>Financial Compound Interest Calculator</h3>
+            <a href="https://maysentg.github.io/Compound-Growth-JS-App/" target="_blank" rel="noreferrer"><img src={CmpdIntrst} className="project-image" alt="supermarket price finder program"/></a>
+            <h2>Skills used</h2>
+            <ul>
+              <li>Javascript (NodeJS, ChartJS)</li>
+              <li>HTML/CSS</li>
             </ul>
           </div>
         </main>
