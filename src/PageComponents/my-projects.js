a00014d258a01b6ffcce2cda4c0a73bc364ece78
MaysenTG <greenwood.maysen@gmail.com>
2021-08-27 15:20:54 +1200

    add overlay scroll bar to avoid webpage shifts with normal scroll bars

--- src/PageComponents/my-projects.js
+++ src/PageComponents/my-projects.js
@@ -5,8 +5,13 @@ import MyWebsites from './Projects/websites';
 import MyAutomation from './Projects/automation';
 import MyAppProjects from './Projects/app-projects';
 
+import 'overlayscrollbars/css/OverlayScrollbars.css';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
+
+
 function MyProjects() {
     return (
+      <OverlayScrollbarsComponent>
           <Tabs fill defaultActiveKey="websites" id="projects-tab" className="mb-3">
             <Tabs.Tab eventKey="websites" title="Websites">
               <MyWebsites />
@@ -18,6 +23,7 @@ function MyProjects() {
               <MyAppProjects />
             </Tabs.Tab>
           </Tabs>
+      </OverlayScrollbarsComponent>
     )
 }
 
