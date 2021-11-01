0295ee27477fcf7e2ad2d579079add1ea81c77ea
MaysenTG <greenwood.maysen@gmail.com>
2021-11-01 15:23:36 +1300

    remove unused packages

--- src/PageComponents/IndexPage.js
+++ src/PageComponents/IndexPage.js
@@ -1,11 +1,8 @@
-import portrait from '../media/self-portrait.webp';
+import portrait from "../media/self-portrait.webp";
 
-import React from 'react';
-import {
-    Link
-} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-
 
 const StyledLink = styled(Link)`
   border: none;
@@ -16,18 +13,18 @@ const StyledLink = styled(Link)`
 function IndexPage() {
   return (
     <main id="index-main">
-        <div className="heading-info main-index">
-          <div id="main-text">
-            <h1>Maysen Greenwood</h1>
-            <h3>Fullstack Developer</h3>
-            <button id="view-projects"><StyledLink to="/my-projects">View my projects</StyledLink></button>
-          </div>
-          <img src={portrait} alt="self portrait of Maysen Greenwood" />
+      <div className="heading-info main-index">
+        <div id="main-text">
+          <h1>Maysen Greenwood</h1>
+          <h3>Fullstack Developer</h3>
+          <StyledLink to="/my-projects">
+            <button id="view-projects">View my projects</button>
+          </StyledLink>
         </div>
+        <img src={portrait} alt="self portrait of Maysen Greenwood" />
+      </div>
     </main>
   );
 }
 
 export default IndexPage;
-
-
