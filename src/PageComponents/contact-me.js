34f2f8a39891242786ac01da22da2262ba832e75
MaysenTG <greenwood.maysen@gmail.com>
2021-11-01 14:12:36 +1300

    remove uneeded images and add social contact links to contact me page

--- src/PageComponents/contact-me.js
+++ src/PageComponents/contact-me.js
@@ -1,40 +1,43 @@
-import React, { useState } from 'react';
-import FormModal from './Modal';
-import Form from 'react-bootstrap/Form'
-import '../index.css';
-import Button from 'react-bootstrap/Button';
-import{ init } from 'emailjs-com';
+import React, { useState } from "react";
+import FormModal from "./Modal";
+import Form from "react-bootstrap/Form";
+import "../index.css";
+import Button from "react-bootstrap/Button";
+import { init } from "emailjs-com";
+
+import { LinkedIn, Facebook } from "@mui/icons-material";
+
+//Initializing data for EmailJS
 init("user_VLCsnAJC6eTea1jTr6ZJo");
 
 function ContactMe() {
   const initialFormData = Object.freeze({
-    fname:"",
-    email:"",
-    message:""
+    fname: "",
+    email: "",
+    message: "",
   });
-  
+
   const sendFeedback = (serviceID, templateId, variables) => {
-    window.emailjs.send(
-        serviceID, templateId,
-        variables
-    ).then(res => {
-        console.log('Email successfully sent!')
-    })
-        .catch(err => console.error('There has been an Error.', err))
-  }
-  
+    window.emailjs
+      .send(serviceID, templateId, variables)
+      .then((res) => {
+        console.log("Email successfully sent!");
+      })
+      .catch((err) => console.error("There has been an Error.", err));
+  };
+
   const [visible, setVisible] = useState(false);
   const [validated, setValidated] = useState(false);
   const [formData, updateFormData] = useState(initialFormData);
-  
+
   const handleChange = (e) => {
     updateFormData({
       ...formData,
 
-      [e.target.name]: e.target.value.trim()
+      [e.target.name]: e.target.value.trim(),
     });
   };
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.currentTarget;
@@ -43,39 +46,73 @@ function ContactMe() {
     }
     setValidated(true);
 
-    const templateId = 'template_olzhave';
-    const serviceID = 'service_wjf5wfb';
-    sendFeedback(serviceID, templateId, {from_name: formData.fname, email: formData.email, message_html: formData.message});
-    
-    setVisible(true);
-    };
-    
-    return (
-      <>
-        <main>
-          <div className="contact__page">
-            <h1 id="contact__title">Contact me!</h1>
-            <Form validated={validated} onSubmit={handleSubmit} className="contact__form">
-              <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Label column="lg">Full Name</Form.Label>
-                <Form.Control onChange= {handleChange} required type="name" name="fname" placeholder="Enter full name"/>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Label column="lg">Email address</Form.Label>
-                <Form.Control onChange= {handleChange} required type="email" name="email" placeholder="Enter email"/>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formGroupPassword">
-                <Form.Label column="lg">Message</Form.Label>
-                <Form.Control onChange= {handleChange} as="textarea" required name="message" placeholder="Enter a message" style={{height: '100px', maxHeight: '250px'}}/>
-              </Form.Group>
-              <Button className="form__button" type="submit">Submit form</Button>
-            </Form>
-            <FormModal show={visible} onHide={() => setVisible(false)}/>
-          </div>
-        </main>
-      </>
-    )
-}
+    const templateId = "template_olzhave";
+    const serviceID = "service_wjf5wfb";
+    sendFeedback(serviceID, templateId, {
+      from_name: formData.fname,
+      email: formData.email,
+      message_html: formData.message,
+    });
 
+    setVisible(true);
+  };
+
+  return (
+    <>
+      <main>
+        <div className="contact__page">
+          <div className="contact">
+            <h1 id="contact__title">Contact me!</h1>
+            <div className="social-links">
+              <a href="https://www.linkedin.com/in/maysen-greenwood/" target="_blank" rel="noreferrer"><LinkedIn fontSize="large" style={{fill: "#0e76a8"}}/></a>
+              <a href="https://www.facebook.com/maysen.greenwood/" target="_blank" rel="noreferrer"><Facebook fontSize="large" style={{fill: "#4267B2"}}/></a>
+            </div>
+          </div>
+          <Form
+            validated={validated}
+            onSubmit={handleSubmit}
+            className="contact__form"
+          >
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label column="lg">Full Name</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                required
+                type="name"
+                name="fname"
+                placeholder="Enter full name"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label column="lg">Email address</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                required
+                type="email"
+                name="email"
+                placeholder="Enter email"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupPassword">
+              <Form.Label column="lg">Message</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                as="textarea"
+                required
+                name="message"
+                placeholder="Enter a message"
+                style={{ height: "100px", maxHeight: "250px" }}
+              />
+            </Form.Group>
+            <Button className="form__button" type="submit">
+              Submit
+            </Button>
+          </Form>
+          <FormModal show={visible} onHide={() => setVisible(false)} />
+        </div>
+      </main>
+    </>
+  );
+}
 
 export default ContactMe;
